0f1f54e22f4df8d3e90e236e8e01c686595fd302
Haolun <84211948+weihaolun@users.noreply.github.com>
2021-09-11 15:26:28 -1000

    Update charts.js

--- static/js/charts.js
+++ static/js/charts.js
@@ -1,11 +1,9 @@
 function init() {
   // Grab a reference to the dropdown select element
-  var selector = d3.select("#selDataset");
-  var samplesfile = https://raw.githubusercontent.com/weihaolun/belly-button-biodiversity/main/samples.json
-  
+  var selector = d3.select("#selDataset");  
 
   // Use the list of sample names to populate the select options
-  d3.json("samplesfile").then((data) => {
+  d3.json("https://raw.githubusercontent.com/weihaolun/belly-button-biodiversity/main/samples.json").then((data) => {
     var sampleNames = data.names;
 
     sampleNames.forEach((sample) => {
@@ -34,7 +32,7 @@ function optionChanged(newSample) {
 
 // Demographics Panel 
 function buildMetadata(sample) {
-  d3.json("samplesfile").then((data) => {
+  d3.json("https://raw.githubusercontent.com/weihaolun/belly-button-biodiversity/main/samples.json").then((data) => {
     var metadata = data.metadata;
     // Filter the data for the object with the desired sample number
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
@@ -62,7 +60,7 @@ function buildMetadata(sample) {
 // 1. Create the buildCharts function.
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
-  d3.json("samplesfile").then((data) => {
+  d3.json("https://raw.githubusercontent.com/weihaolun/belly-button-biodiversity/main/samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
     var samples = data.samples;
     // 4. Create a variable that filters the samples for the object with the desired sample number.
